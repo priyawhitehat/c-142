e1ca98ca3fff7f2bad6f9bc7e03e92f6f253cbec
priyawhitehat <73846406+priyawhitehat@users.noreply.github.com>
2022-10-07 17:50:53 +0530

    Update merge_csv.py

--- merge_csv.py
+++ merge_csv.py
@@ -1,12 +1,2 @@
 
 
-for movie_item in all_movies:
-    poster_found = any(movie_item[8] in movie_link_items for movie_link_items in all_movie_links)
-    if poster_found:
-        for movie_link_item in all_movie_links:
-            if movie_item[8] == movie_link_item[0]:
-                movie_item.append(movie_link_item[1])
-                if len(movie_item) == 28:
-                    with open("final.csv", "a+") as f:
-                        csvwriter = csv.writer(f)
-                        csvwriter.writerow(movie_item)
